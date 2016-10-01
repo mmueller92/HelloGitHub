ce703522fe44808b31a33c2570d966f34a0a352e
Manuel-PC\Manuel <m.mueller@edu.campus02.at>
2016-10-01 13:35:59 +0200

    test

---
+++ HelloGitHub/src/OperatorsArithmetic.java
@@ -0,0 +1,30 @@
+
+public class OperatorsArithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=5;
+		
+		
+		//a = a+1;
+		//a+=1;
+		
+		
+		int result =a+b;
+		
+		System.out.println(a+" + " +b + " = " + result);
+		
+		result = a - b;
+		System.out.println(a+" - " + b + " = " + result);
+	
+		//System.out.println(a-b);
+		//System.out.println(a*b);
+		//System.out.println(a/b);
+		//System.out.println(a%b);
+
+	}
+	
+	
+}
---
+++ HelloGitHub/src/Übung3Temperatur.java
@@ -0,0 +1,9 @@
+
+public class Übung3Temperatur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
